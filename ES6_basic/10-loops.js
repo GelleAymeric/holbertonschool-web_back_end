107b07d655d6eb377ac9997c2ca8011ea7d50169
Aymeric154 <aymeric.gelle@outlook.fr>
2024-11-11 15:49:14 +0100

    Disable ESLint for multiple ES6 files to enhance code readability

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
     for (const value of array) {
       array[array.indexOf(value)] = appendString + value;
