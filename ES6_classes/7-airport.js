25e584ca5a12251148b7ed5170b4fa100b392097
Aymeric154 <aymeric.gelle@outlook.fr>
2024-11-11 15:24:31 +0100

    Refactor Airport class: improve formatting, add toString method, and implement Symbol.toStringTag

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -2,25 +2,29 @@ export default class Airport {
     constructor(name, code) {
         this._name = name;
         this._code = code;
-    }
+        }
 
     get name() {
-        return this._name;
+    return this._name;
     }
 
     get code() {
-        return this._code;
+    return this._code;
     }
 
     set name(name) {
-        this._name = name;
+    this._name = name;
     }
-
+        
     set code(code) {
-        this._code = code;
+    this._code = code;
     }
 
-    display() {
-        return `${this._name} (${this._code})`;
+    get [Symbol.toStringTag]() {
+    return 'Airport';
+    }
+
+    toString() {
+    return `[object ${this._code}]`;
     }
 }
