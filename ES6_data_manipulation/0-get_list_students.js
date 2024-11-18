b1d2f27c194d4a2bf6d0c70b956c9b816d828c81
Aymeric154 <aymeric.gelle@outlook.fr>
2024-11-18 09:40:40 +0100

    Add initial implementations for student data manipulation functions

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+    return [
+        { id: 1, firstName: 'John', location: 'San Francisco' },
+        { id: 2, firstName: 'Jane', location: 'New York' },
+        { id: 3, firstName: 'Tom', location: 'Chicago' },
+    ];
+}
