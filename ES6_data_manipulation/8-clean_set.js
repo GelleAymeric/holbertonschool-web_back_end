c876c8fb9626c7ac08dfb3913f3aad8084a3a5b2
Aymeric154 <aymeric.gelle@outlook.fr>
2024-11-18 12:38:02 +0100

    Refactor cleanSet function to improve input validation and enhance readability

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,15 @@
 /* eslint-disable */
 export default function cleanSet(set, startString) {
-    let result = [];
-  
-    for (let item of set) {
-      if (item.startsWith(startString) && startString !== '') {
-        result.push(item.slice(startString.length));
-      }
+    if (typeof startString !== 'string' || startString.length === 0) {
+        return '';
     }
-    
+
+    const result = [];
+    for (const value of set) {
+        if (value.startsWith(startString)) {
+            result.push(value.slice(startString.length));
+        }
+    }
+
     return result.join('-');
-  }
+}
