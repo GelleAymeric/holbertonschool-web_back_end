d755ce66f407293ba24b7b6979018a0998f1dcea
Aymeric154 <aymeric.gelle@outlook.fr>
2024-11-18 11:19:55 +0100

    Fix variable name in createInt8TypedArray function and add hasValuesFromArray function

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default function createInt8TypedArray(lenght, position, value) {
     const buffer = new ArrayBuffer(lenght);
-    const int8View = new Int8Array(buffer);
+    const int8 = new Int8Array(buffer);
     if (position >= lenght) {
         throw Error('Position outside range');
     }
-    int8View[position] = value;
+    int8[position] = value;
     return new DataView(buffer);
 }
