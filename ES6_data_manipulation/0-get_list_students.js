c103a198b77c059f297442e8317a943bd0acf190
Aymeric154 <aymeric.gelle@outlook.fr>
2024-11-18 10:06:04 +0100

    Update student names and locations in getListStudents function

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 export default function getListStudents() {
     return [
-        { id: 1, firstName: 'John', location: 'San Francisco' },
-        { id: 2, firstName: 'Jane', location: 'New York' },
-        { id: 5, firstName: 'Tom', location: 'Chicago' },
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' },
     ];
 }
