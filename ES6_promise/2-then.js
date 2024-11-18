d3f05c12109335b57476b5838a90a1c4b89fd13f
Aymeric154 <aymeric.gelle@outlook.fr>
2024-11-18 13:10:20 +0100

    Enhance handleResponseFromAPI to return success status and handle errors

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,14 @@
 
 export default function handleResponseFromAPI(promise) {
     return promise
-        .then(function(promise) {
+        .then(function(
+            
+        ) {
             console.log('Got a response from the API');
+            return { status: 200, body: 'success' };
         })
+        .catch(() => {
+            console.log('Got a response from the API');
+            return new Error();
+        });
 }
