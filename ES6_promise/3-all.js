a6511797e8d42bfc300736308d8fad624cbab2b0
Aymeric154 <aymeric.gelle@outlook.fr>
2024-11-18 13:38:13 +0100

    Refactor handleProfileSignup to improve readability and add user promise function for signup

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,13 +1,13 @@
-/* eslint-enable */
-import { uploadPhoto, createUser } from './utils';
+/* eslint-disable */
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
-    .then((userResponse) => {
-        const [photo , user] = userResponse;
-        console.log(`${photo.body}, ${user.firstName}, ${user.lastName}`);
-    })
-    .catch(() => {
-        console.log('Signup system offline');
-    });
+        .then((results) => {
+            const [photo, user] = results;
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        })
+        .catch(() => {
+            console.log('Signup system offline');
+        });
 }
