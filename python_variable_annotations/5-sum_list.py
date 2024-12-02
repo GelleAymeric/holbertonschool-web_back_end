9b6d5ce27508eb85ca6dc61c692221c35114c32c
Aymeric154 <aymeric.gelle@outlook.fr>
2024-12-02 11:58:52 +0100

    Add type annotation for sum_list function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""" type annotation for sum_list function """
 
 
 def sum_list(input_list: float) -> float:
+    """ type annotation for sum_list function """
     return sum(input_list)
