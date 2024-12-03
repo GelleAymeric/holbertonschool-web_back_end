bf2f61b946bea9379b2befeaa6c9f1801dc45c03
Aymeric154 <aymeric.gelle@outlook.fr>
2024-12-03 08:51:21 +0100

    Add asynchronous functions to handle random delays and concurrent execution

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,11 @@
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Return the list of all the delays"""
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
