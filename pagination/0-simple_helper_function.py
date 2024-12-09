3134eebf8c33f0b2afe90df93282c32380e68670
Aymeric154 <aymeric.gelle@outlook.fr>
2024-12-09 11:13:57 +0100

    Fix index_range calculation to correctly return start and end indices

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,5 +3,4 @@
 
 def index_range(page: int, page_size: int) -> tuple:
     """return a tuple of size two containing a start index and an end index"""
-    return page * page_size - page_size, page * page_size
-    
+    return ((page - 1) * page_size, page * page_size)
