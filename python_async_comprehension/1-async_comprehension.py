ec19c7111222111dae333023aed4e64ac53e2bad
Aymeric154 <aymeric.gelle@outlook.fr>
2024-12-09 11:03:08 +0100

    Add type annotation for async_comprehension return value

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,6 @@
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> list[float]:
     """Collect 10 random numbers using an async comprehensing"""
     return [i async for i in async_generator()]
