67c38fe39fb43b70024135029ef55bb5e8fd011d
Aymeric154 <aymeric.gelle@outlook.fr>
2024-12-09 10:59:24 +0100

    Add return type annotations for async_comprehension and measure_runtime functions

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,7 +6,7 @@ from time import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """Measure the runtime of async_comprehension four times in parallel"""
     start = time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
