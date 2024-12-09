1c47380231dd748c1d35544785390f021295e6f2
Aymeric154 <aymeric.gelle@outlook.fr>
2024-12-09 10:32:01 +0100

    Add async generator and comprehension functions with runtime measurement

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""""Measure the runtime of async_comprehension four times in parallel"""
+
+import asyncio
+from time import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """Measure the runtime of async_comprehension four times in parallel"""
+    start = time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = time()
+    return end - start
