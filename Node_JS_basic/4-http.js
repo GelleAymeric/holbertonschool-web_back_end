651e852d027e28eb1c16760b5222b97e79b1c293
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-20 14:35:46 +0100

    Refactor HTTP server to set response headers and log server status on startup

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,12 +1,13 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
-    }
-  });
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
+});
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
 
 module.exports = app;
