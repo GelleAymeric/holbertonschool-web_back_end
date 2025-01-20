22e47beb2ac3cf8c4854358fbf1c072f8a91b6e5
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-20 09:34:38 +0100

    Add JSDoc comments to displayMessage function for better documentation

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -3,6 +3,10 @@
  * 
  **/
 
+/**
+ * Logs the provided message to the console.
+ * @param {string} message - The message to be displayed.
+ */
 function displayMessage(message) {
     console.log(message);
 }
