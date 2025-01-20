b803842967d88d1a0b6b408ac7507511fce13eb4
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-20 09:53:25 +0100

    Refactor displayMessage function documentation for clarity

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,11 +1,9 @@
 /**
- * Logs the provided message to the console.
- * 
+ * Function to display a message in the console.
  * @param {string} message - The message to be displayed.
  */
 function displayMessage(message) {
     console.log(message);
 }
-
 // Export the function.
 module.exports = displayMessage;
