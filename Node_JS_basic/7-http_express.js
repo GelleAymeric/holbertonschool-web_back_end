fe1bca1d4b9a4b75da54185f40684ec923d8a285
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-20 15:10:26 +0100

    Refactor student retrieval endpoint to use async/await and improve error handling

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,36 +1,29 @@
 const express = require('express');
-const fs = require('fs');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
 
 const app = express();
-const database = process.argv[2];
+const port = 1245;
 
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
-
-app.get('/students', (req, res) => {
-  if (!database) return res.status(500).send('This is the list of our students\nCannot load the database');
-
-  fs.readFile(database, 'utf8', (err, data) => {
-    if (err) return res.status(500).send('This is the list of our students\nCannot load the database');
-
-    const students = data.split('\n').filter((line) => line.trim() !== '').slice(1);
-    const fields = {};
-
-    students.forEach((student) => {
-      const [firstname, , , field] = student.split(',');
-      fields[field] = fields[field] || [];
-      fields[field].push(firstname);
-    });
-
-    let response = 'This is the list of our students\n';
-    response += `Number of students: ${students.length}\n`;
-    Object.entries(fields).forEach(([field, names]) => {
-      response += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
-    });
-
-    res.send(response);
-  });
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.listen(1245, () => console.log('Server is running on port 1245'));
+app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
+});
+
+app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+});
 
 module.exports = app;
