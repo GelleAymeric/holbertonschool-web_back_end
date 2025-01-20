eb8ee94d6ed8e05df21df6c250198d8fae9e4a75
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-20 14:31:27 +0100

    Fix indentation in HTTP server response handling

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,11 +1,11 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('Hello Holberton School!');
-        res.end();
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
     }
-});
+  });
 
 app.listen(1245);
 
