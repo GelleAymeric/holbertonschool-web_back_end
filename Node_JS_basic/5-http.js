782fb49bf1cad2400491847c0eb14a86ad34fc42
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-20 15:00:14 +0100

    Refactor HTTP server to use async/await for student list retrieval and improve response handling

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,58 +1,36 @@
 const http = require('http');
-const fs = require('fs');
 
-const app = http.createServer((req, res) => {
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
-  if (req.url === '/') {
-    res.statusCode = 200;
-    res.end('Hello Holberton School!\n');
-  } else if (req.url === '/students') {
-    const database = process.argv[2];
+  const { url } = req;
 
-    if (!database) {
-      res.statusCode = 500;
-      res.end('This is the list of our students\nCannot load the database');
-      return;
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
     }
-
-    fs.readFile(database, 'utf8', (err, data) => {
-      if (err) {
-        res.statusCode = 500;
-        res.end('This is the list of our students\nCannot load the database');
-        return;
-      }
-
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1); // Ignorer la première ligne (en-tête)
-
-      const fields = {};
-      students.forEach((student) => {
-        const [firstname, , , field] = student.split(',');
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-      });
-
-      let response = 'This is the list of our students\n';
-      response += `Number of students: ${students.length}\n`;
-
-      for (const [field, names] of Object.entries(fields)) {
-        response += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
-      }
-
-      res.statusCode = 200;
-      res.end(response);
-    });
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found\n');
   }
+  res.statusCode = 404;
+  res.end();
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
