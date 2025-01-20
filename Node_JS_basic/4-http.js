6ee8adfe0683bfe26bbd96430956d06b168b6312
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-20 14:26:16 +0100

    Implement basic HTTP server to respond with a greeting message

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello Holberton School!');
+        res.end();
+    }
+});
+
+app.listen(1245);
+
+module.exports = app;
