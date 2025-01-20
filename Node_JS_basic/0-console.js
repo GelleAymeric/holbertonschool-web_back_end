03a63a2ddfbde1b8bddab74da2b28261e55cc146
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-20 09:37:07 +0100

    Add JSDoc comments and export displayMessage function in console.js

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,9 +1,12 @@
 /**
  * Logs the provided message to the console.
+ * 
  * @param {string} message - The message to be displayed.
  */
+
 function displayMessage(message) {
     console.log(message);
 }
 
+// Export the function.
 module.exports = displayMessage;
