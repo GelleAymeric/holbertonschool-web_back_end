40eebca9762c9843e54463ff14cf1cc504709e97
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-21 09:03:24 +0100

    Add async handling and error response for /students route in HTTP server

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -8,22 +8,30 @@ const DATABASE = args[0];
 const app = express();
 const port = 1245;
 
+// Define a route handler for the root URL path
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
+// Define a route handler for the /students URL path
 app.get('/students', async (req, res) => {
   const msg = 'This is the list of our students\n';
   try {
+    // Call the countStudents function and await its result
     const students = await countStudents(DATABASE);
+    // Send the response with the list of students
     res.send(`${msg}${students.join('\n')}`);
   } catch (error) {
+    // Send the response with the error message if an error occurs
     res.send(`${msg}${error.message}`);
   }
 });
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
-  //   console.log(`Example app listening at http://localhost:${port}`);
+  // Uncomment the line below to log a message when the server starts
+  // console.log(`Example app listening at http://localhost:${port}`);
 });
 
+// Export the app for use in other files
 module.exports = app;
