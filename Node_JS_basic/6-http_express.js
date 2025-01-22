ac486ce49e7c0bbda119888329bb936ca3713fcf
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-22 09:53:53 +0100

    Add root route handler and server initialization in Express app

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
+// Define a route handler for the root URL path
 app.get('/', (req, res) => {
+  // Send a response with the text 'Hello Holberton School!'
   res.send('Hello Holberton School!');
 });
 
+// Start the server and listen on port 1245
 app.listen(1245);
 
+// Export the app for use in other files
 module.exports = app;
